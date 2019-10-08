e1db65d6f28d112f8ebdb2612142d3a5513e79b2
Pradip Caulagi <caulagi@gmail.com>
2019-10-08 21:26:07 +0200

    Run gofmt on each commit (#2)
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

---
+++ scripts/gofmtcheck.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "==> Checking that code complies with gofmt requirements..."
+gofmt_files=$(gofmt -l `find . -name '*.go' | grep -v vendor`)
+if [[ -n ${gofmt_files} ]]; then
+    echo 'gofmt needs running on the following files:'
+    echo "${gofmt_files}"
+    exit 1
+fi
+
+exit 0
