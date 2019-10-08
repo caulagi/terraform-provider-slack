4680ec586b32f9daa7e80411b26756a0e069d06c
Pradip Caulagi <caulagi@gmail.com>
2019-10-08 21:24:38 +0200

    Revert "Break build"
    This reverts commit 6677af2d46da0ba1af8c721f48efe488ec89dce7.

--- provider.go
+++ provider.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
-func   Provider() *schema.Provider {
+func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 			"slack": resourceServer(),
